f818ff0fe2a7963a84300c59fac973627c88074b
Fernando Schwengber <124198375+Fernandoakafox@users.noreply.github.com>
2024-05-10 10:31:35 -0300

    Update casoDeUso.py

--- casoDeUso.py
+++ casoDeUso.py
@@ -1,6 +1,6 @@
 import transformToAlternativeText as tTAT
 
-text = "Explique a imagem para um cego. Ele receberá esta imagem, e deve entende-la. Sua explicação será utilizada como alternative text, portanto, siga boas praticas de alternative text e retorne somente um alternative text."
+text = "Explique a imagem para uma pessoa cega. Esta pessoa receberá esta imagem, e deve entende-la. Sua explicação será utilizada como alternative text, portanto, siga boas praticas de alternative text e retorne somente um alternative text."
 
 # criação de um objeto iTAT, passando apikey do google e texto de prompt
 object_tTAT = tTAT.ImageToAltertiveText("API-KEY-AQUI",textPrompt=text)
